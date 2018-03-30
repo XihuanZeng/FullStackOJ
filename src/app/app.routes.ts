7540f63b2a1ef66854e31bfd958b1f93dfed328f
Xihuan Zeng <xzeng@fulcrm.com>
2018-03-30 10:53:20 -0400

    initial commit that have all starter files

---
+++ src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes, RouterModule, RouterOutlet } from '@angular/router';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+
+// a list of objects
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'problemes',
+        pathMatch: 'full' // exact match
+    },
+    {
+        path: 'problems',
+        component: ProblemListComponent
+    },
+    {
+        path: 'problems/:id',
+        component: ProblemDetailComponent
+    },
+    {
+        // handling random url 
+        path: '**',
+        redirectTo: 'problems'
+    }
+]
+
+export const routing = RouterModule.forRoot(routes);
