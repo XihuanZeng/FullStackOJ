80584fed5a86f682a0f3721436b172a48018d586
XihuanZeng <zengx272@gmail.com>
2018-04-05 23:35:32 -0400

    edit package.json to have nodemon start with npm

--- oj-server/server.js
+++ oj-server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const restRouter = require('./routes/rest');
 const mongoose = require('mongoose');
 
+app.get('/', (req, res) => res.send('Hello World!!!'));
 app.use('/api/v1', restRouter);
 
 // connection string is mongodb://<dbuser>:<dbpassword>@ds151707.mlab.com:51707/xzproblems
@@ -13,3 +14,5 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'));
 // other functions of this app are app.get('/', function(req, res){
 //  res.send('hello world');
 //  }))
+
+
--- oj-server/services/problemService.js
+++ oj-server/services/problemService.js
@@ -34,3 +34,51 @@ let problems = [
 // Note that why we don't need this information, because they are already in MongoDB
 
 const ProblemModel = require('../models/problemModel');
+
+
+const getProblems = function() {
+    return new Promise((resolve, reject) => {
+      ProblemModel.find({}, function(err, problems) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(problems);
+        }
+      })
+    })
+  }
+
+  const getProblem = function(id) {
+    return new Promise((resolve, reject) => {
+      ProblemModel.findOne({id: id}, function(err, problem) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(problem);
+        }
+      })
+    })
+  }
+
+  const addProblem = function(newProblem) {
+    return new Promise((resolve, reject) => {
+      ProblemModel.findOne({name: newProblem.name}, function(err, data){
+        if (data) {
+          reject('Problem name already exists');
+        } else {
+          ProblemModel.count({}, function(err, num) {
+            newProblem.id = num + 1;
+            let mongoProblem = new ProblemModel(newProblem);
+            mongoProblem.save();
+            resolve(mongoProblem);
+          })
+        }
+      })
+    });
+  }
+
+  module.exports = {
+    getProblems,
+    getProblem,
+    addProblem
+  }
